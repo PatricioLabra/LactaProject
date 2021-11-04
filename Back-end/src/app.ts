663d987bf6228b5487fec1f19f910a68b1e18e54
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-04 15:32:18 -0300

    agregue un comentario y el middleware de morgan

--- Back-end/src/app.ts
+++ Back-end/src/app.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
+//Config variables
 const app = express();
 
 // settings
 app.set('port', process.env.PORT || 4000);
+
+//Middlewares
+app.use(morgan('dev'));
 
 export default app;
