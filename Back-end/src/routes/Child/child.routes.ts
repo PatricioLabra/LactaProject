8000f8ff3f9f579ccd13815d61f5f5d1d6daf865
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-04 15:56:37 -0300

    configuracion inicial en la carpeta Child

---
+++ Back-end/src/routes/Child/child.routes.ts
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+import * as productCtrl from './child.controller';
+
+const router = Router();
+
+//agregar nuevo lactante
+router.get('/child',);
+
+export default router
