5e12e2727edc5babe158ff6ef55252c7cd85c834
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-04 21:53:10 -0300

    se agregan los endpoints de las asesoradas

---
+++ Back-end/src/routes/Mother/mother.routes.ts
@@ -0,0 +1,21 @@
+import {Router} from 'express';
+import * as motherlCtrl from './mother.controller';
+
+const router = Router();
+
+//agregar nueva asesorada 
+router.post('/mother', );
+
+//modificar una asesorada
+router.put('/mother/:id, ')
+
+//eliminar una asesorada
+router.delete('/mother/:id')
+
+//obtener asesorada
+router.get('/mother/:id')
+
+//obtener todas las asesoradas
+router.get('/mother')
+
+export default router
