6b0fb0accec7dbf08503540bff3d43f83ae2a1c4
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 18:49:07 -0300

    arreglos en el archivo admin.routes.ts

--- Back-end/src/routes/Admin/admin.routes.ts
+++ Back-end/src/routes/Admin/admin.routes.ts
@@ -4,12 +4,12 @@ import * as adminCtrl from './admin.controller';
 const router = Router();
 
 // Agregar un nuevo admin
-router.post('/admin/signup',);
+router.post('/admin/signup');
 
 // Obtener la informacion de un admin
-router.get('/admin/:nick',);
+router.get('/admin/:nick');
 
 // Inicia sesión
-router.post('/admin/signin', );
+router.post('/admin/signin');
 
 export default router;
