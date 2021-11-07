40aa07cd35588974dbca10ac311509927ec350d0
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 20:04:47 -0300

    se agrega texto de prueba en la funcion signup

--- Back-end/src/routes/Admin/admin.controller.ts
+++ Back-end/src/routes/Admin/admin.controller.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
+import { createHmac } from "crypto";
 
 export const signUp: RequestHandler = async (req, res) => {
-    
+    return res.send('todo ok prueba');   
 }
