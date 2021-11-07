37a8c181bb2c84e4082e788bd4e619d6378c4b03
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 18:57:45 -0300

    se agregaron los cambios en child.routes.ts y el espacio en admin.routes.ts

--- Back-end/src/routes/Child/child.routes.ts
+++ Back-end/src/routes/Child/child.routes.ts
@@ -4,18 +4,18 @@ import * as childCtrl from './child.controller';
 const router = Router();
 
 //agregar nuevo lactante
-router.post('/child', );
+router.post('/child');
 
 //modificar Lactante
-router.put('/child/:id, ')
+router.put('/child/:id');
 
 //eliminar Lactante
-router.delete('/child/:id')
+router.delete('/child/:id');
 
 //obtener Lactante
-router.get('/child/:id')
+router.get('/child/:id');
 
-//obtener Lista Lactantes
-router.get('/child')
+//obtener lista Lactantes
+router.get('/child');
 
 export default router
