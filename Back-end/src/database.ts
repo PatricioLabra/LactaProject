ffa891496a0f366ddde5dc3469e48e463f43e28a
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 20:53:29 -0300

    funciona la coneccion a la base de datos

--- Back-end/src/database.ts
+++ Back-end/src/database.ts
@@ -2,8 +2,13 @@ import mongoose, { ConnectOptions } from 'mongoose'
 import dotenv from 'dotenv';
 dotenv.config();
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ncdk5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
-mongoose
-    .connect(uri)
-    .then(() => console.log('Base de datos conectada'))
-    .catch(e => console.log('error db:', e))
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.vtdaa.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+export async function startConnection() {
+    try {
+      const db = await mongoose.connect(uri);
+      console.log('Database is connected');
+    } catch (error) {
+      console.log(error);
+    }
+  }
--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -1,7 +1,9 @@
 import app from './app';
-
+import { startConnection } from './database';
 
 function main(){
+    startConnection();
+
     app.listen(app.get('port'));
     console.log('Server running on port', app.get('port'));
 }
