e851f73f7e8b4385b8954d5baa3b73d8953c95c7
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 19:02:54 -0300

    se arreglo el archivo mother.routes.ts

--- Back-end/src/routes/Mother/mother.routes.ts
+++ Back-end/src/routes/Mother/mother.routes.ts
@@ -4,18 +4,18 @@ import * as motherlCtrl from './mother.controller';
 const router = Router();
 
 //agregar nueva asesorada 
-router.post('/mother', );
+router.post('/mother');
 
 //modificar una asesorada
-router.put('/mother/:id, ')
+router.put('/mother/:id');
 
 //eliminar una asesorada
-router.delete('/mother/:id')
+router.delete('/mother/:id');
 
 //obtener asesorada
-router.get('/mother/:id')
+router.get('/mother/:id');
 
 //obtener todas las asesoradas
-router.get('/mother')
+router.get('/mother');
 
 export default router
