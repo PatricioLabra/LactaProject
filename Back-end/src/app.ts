b8906b090608114b2436964fde4e1f96ae8813c1
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 19:40:12 -0300

    se importa el dotenv en el index.ts

--- Back-end/src/app.ts
+++ Back-end/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 // Internal imports
 import indexRoutes from './routes/index.routes';
