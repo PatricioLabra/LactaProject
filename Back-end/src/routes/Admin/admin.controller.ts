cc9f5aada2b80e758448506e7c76b48bb3e484ff
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 21:35:20 -0300

    se agrego un comentario de prueba en la funcion signUp

--- Back-end/src/routes/Admin/admin.controller.ts
+++ Back-end/src/routes/Admin/admin.controller.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import { createHmac } from "crypto";
 import Admin from './admin.model'
 
+
 export const signUp: RequestHandler = async (req, res) => {
-     
+    res.send('asdasdasd')
 }
