b467c6e2bdc24d57473dba7171b08a38b2d4cc7d
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 21:07:30 -0300

    se agrega un schema temporal para empezar a trabajar

---
+++ Back-end/src/routes/Admin/admin.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const adminSchema = new Schema({
+	rut: {
+    required: true,
+    type: String,
+    trim: true,
+    min: 12,
+  },
+  password: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  email: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  permission_level: {
+      type: Number,
+      required: true
+  }
+}, {
+  versionKey: false,
+  timestamps: true
+});
+
+export default model('Admin', adminSchema);
