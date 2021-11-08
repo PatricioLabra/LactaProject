6e9473c77d632611cf83ee18e98325aaa79f8956
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 21:44:22 -0300

    se hicieron arreglos para que me aprueben el pr a la primera :v

--- Back-end/src/routes/Admin/admin.controller.ts
+++ Back-end/src/routes/Admin/admin.controller.ts
@@ -1,8 +1,8 @@
 import { RequestHandler } from "express";
 import { createHmac } from "crypto";
-import Admin from './admin.model'
+import Admin from './admin.model';
 
 
 export const signUp: RequestHandler = async (req, res) => {
-    res.send('asdasdasd')
+    res.send('asdasdasd');
 }
