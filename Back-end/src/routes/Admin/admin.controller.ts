0d117b392a04fd19581525bd63339938c095c653
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-07 23:42:31 -0300

    se instalo jsonwebtoken como dependencia y se importo en admin controller

--- Back-end/src/routes/Admin/admin.controller.ts
+++ Back-end/src/routes/Admin/admin.controller.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import { createHmac } from "crypto";
 import Admin from './admin.model';
+import jwt from 'jsonwebtoken';
 
 
 export const signUp: RequestHandler = async (req, res) => {
