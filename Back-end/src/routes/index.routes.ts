63cb1f1778734b71d46df73284dcc9027383c4dc
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-09 21:47:39 -0300

    se elimino todo lo relacionado a profesionales ya que no se va a usar

--- Back-end/src/routes/index.routes.ts
+++ Back-end/src/routes/index.routes.ts
@@ -4,7 +4,6 @@ import userRoutes from './User/user.routes'
 import childRoutes from './Child/child.routes';
 import controlRoutes from './Control/control.routes';
 import motherRoutes from './Mother/mother.routes';
-import profesionalRoutes from './Profesional/profesional.routes';
 
 const router = Router();
 
@@ -12,4 +11,4 @@ router.get('/', (req, res) => {
     return res.send('Welcome to my API!');
 });
 
-export default [router, userRoutes,childRoutes, controlRoutes, motherRoutes, profesionalRoutes];
+export default [router, userRoutes,childRoutes, controlRoutes, motherRoutes];
