c6fc40f3d6450d076ff69a32c6a898f1035f3c33
DanRivaille <dan.santos.hd@outlook.com>
2021-11-12 20:21:42 -0300

    Se agrega la variable de entorno API_URL

--- Front-end/lactaProject/src/app/services/api-send.service.ts
+++ Front-end/lactaProject/src/app/services/api-send.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CoreModule } from '@core/core.module';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: CoreModule
 })
 export class ApiSendService {
 
+  API_URL = environment.API_URL;
+
   constructor() { }
+
+  /**
+   * Crea una url hacia la api, con los parametros ingresados
+   * @param listParams Lista de parametros de la url
+   * @returns url hacia la api con los parametros correspondientes 
+   */
+  makeUrl(listParams: Array<string | number>): string {
+    const params = listParams.join('/');
+    const url = `${this.API_URL}/${params}`;
+    return url;
+  }
 }
