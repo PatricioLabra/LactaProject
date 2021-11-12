836a02ed9866f441a6d4dad4ee5b14f67bbb6306
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-11 23:00:14 -0300

    se agrego el modell de child

---
+++ Back-end/src/routes/Child/child.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+
+const childSchema = new Schema({
+  name: {
+    required: true,
+    type: String,
+    trim: true
+  },
+  gestacion_data:{
+    diseases_during_pregnancy:{required:true, type: [String]} ,
+    nutritional_status_mother: {required:true, type: String},
+    planned_pregnancy: {required:true, type: Boolean},
+    assisted_fertilization: {required: true, type: Boolean},
+    previus_lactation: {required: true, type: String},
+    duration_of_past_lactation: {required: true, type: Number},
+    breastfeeding_education: {required: true, type: Boolean}
+  },
+  birth_data:{
+    birthplace: {required:true, type: String},
+    type_of_birth: {required:true, type: String},
+    birthday: {required:true, type: Date},
+    gestional_age: {required:true, type: Number},
+    gender: {required:true, type: String},
+    birth_weight: {required:true, type: Number},
+    skin_to_skin_contact:{required:true, type: Boolean},
+    breastfeeding_b4_2hours:{required:true, type: Boolean},
+    has_suplement:{required: true, type: Boolean},
+    why_recived_suplement:{required:false, type: String},
+    joint_accommodation:{required:true, type: Boolean},
+    use_of_pacifier:{required:true, type: Boolean},
+    post_discharge_feeding:{required:true, type: String},
+    last_weight_control:{required:true, type: Number}
+  },
+  id_mother:{required:true, type: Schema.Types.ObjectId}
+  },
+  {
+    versionKey: false,
+    timestamps: true
+  });
+
+
+export default model('Child', childSchema);
