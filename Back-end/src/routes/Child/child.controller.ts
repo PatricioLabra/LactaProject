8c2c292fc6aa521779922db387bcc83c877ccd24
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-12 12:31:19 -0300

    se agregaron las funciones de child.controller y se edito un pequenho detalle en child.roures

--- Back-end/src/routes/Child/child.controller.ts
+++ Back-end/src/routes/Child/child.controller.ts
@@ -1,1 +1,21 @@
 import { RequestHandler } from "express";
+
+export const newChild: RequestHandler = async (req, res) => {
+    
+}
+
+export const editChild: RequestHandler = async (req, res) => {
+    
+}
+
+export const deleteChild: RequestHandler = async (req, res) => {
+    
+}
+
+export const getResumeChild: RequestHandler = async (req, res) => {
+    
+}
+
+export const getChild: RequestHandler = async (req, res) => {
+    
+}
--- Back-end/src/routes/Child/child.routes.ts
+++ Back-end/src/routes/Child/child.routes.ts
@@ -4,18 +4,18 @@ import * as childCtrl from './child.controller';
 const router = Router();
 
 //agregar nuevo lactante
-router.post('/child/:idMother');
+router.post('/child/:idMother', childCtrl.newChild);
 
 //modificar Lactante
-router.put('/child/:idLactante');
+router.put('/child/:idLactante', childCtrl.editChild);
 
 //eliminar Lactante en la lista de la madre
-router.delete('/child/:idLactante/:idMother');
+router.delete('/child/:idLactante', childCtrl.deleteChild);
 
 //obtener lista Lactante resumido(idMother)
-router.get('/child/:idMother');
+router.get('/child/:idMother', childCtrl.getResumeChild);
 
 //obtener Lactante completo
-router.get('/child/profile/:idLactante');
+router.get('/child/profile/:idLactante', childCtrl.getChild);
 
 export default router
