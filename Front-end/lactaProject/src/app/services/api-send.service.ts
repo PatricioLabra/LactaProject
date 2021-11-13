c6262b620ff50c06c541163035b25adc84593e03
DanRivaille <dan.santos.hd@outlook.com>
2021-11-13 13:01:17 -0300

    Se crea una clase api.class.ts para modularizar los metodos y propiedades de api-send y api-get

--- Front-end/lactaProject/src/app/services/api-send.service.ts
+++ Front-end/lactaProject/src/app/services/api-send.service.ts
@@ -1,25 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 import { CoreModule } from '@core/core.module';
+import { ApiClass } from './api.class';
 
 @Injectable({
   providedIn: CoreModule
 })
-export class ApiSendService {
+export class ApiSendService extends ApiClass {
 
-  API_URL = environment.API_URL;
-
-  constructor(private http: HttpClient) { }
-
-  /**
-   * Crea una url hacia la api, con los parametros ingresados
-   * @param listParams Lista de parametros de la url
-   * @returns url hacia la api con los parametros correspondientes 
-   */
-  makeUrl(listParams: Array<string | number>): string {
-    const params = listParams.join('/');
-    const url = `${this.API_URL}/${params}`;
-    return url;
+  constructor(http: HttpClient) {
+    super(http);
   }
+
 }
---
+++ Front-end/lactaProject/src/app/services/api.class.ts
@@ -0,0 +1,20 @@
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+export class ApiClass {
+
+  readonly API_URL: string = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Crea una url hacia la api, con los parametros ingresados
+   * @param listParams Lista de parametros de la url
+   * @returns url hacia la api con los parametros correspondientes 
+   */
+  protected makeUrl(listParams: Array<string | number>): string {
+    const params = listParams.join('/');
+    const url = `${this.API_URL}/${params}`;
+    return url;
+  }
+}
