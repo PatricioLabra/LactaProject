4726b62d8d95f4bf72db217aba2a422bad4bf1b5
PatricioLabra <patricio.labra827@gmail.com>
2021-11-14 02:28:16 -0300

    se cambian las comillas dobles por simples, se modifica el encabezado y se elimina import que no es usado aún

--- Back-end/src/routes/Mother/mother.controller.ts
+++ Back-end/src/routes/Mother/mother.controller.ts
@@ -1,13 +1,12 @@
 import { RequestHandler } from "express";
 import Mother from './mother.model';
-import { Types } from "mongoose";
 
 
 /**
  * Función que maneja la petición de agregar a una nueva madre al sistema.
  * @route Post /mother
  * @param req Request de la petición, se espera que tenga la información de la nueva madre
- * @param res Response, retorna un un object con succes:true, data:{} y un message: "String" de la nueva madre si todo sale bien
+ * @param res Response, retorna un un object con succes:true, data:{ _id: ObjectId() } y un message: "String" de la nueva madre si todo sale bien
  */
 export const newMother: RequestHandler = async (req, res) => {
     const {
@@ -19,13 +18,13 @@ export const newMother: RequestHandler = async (req, res) => {
     //se valida si alguno de los atributos required no son válidos
     if ( !name || !rut || !commune || !phone_number || !birth || !ocupation || !studies || 
         !marital_status || !forecast || !chronic_diseases || !childs || number_of_living_children < 0)
-        return res.status(400).send({ succes: false, data:{}, message:"ERROR: Datos inválidos" + req.body });
+        return res.status(400).send({ succes: false, data:{}, message:'ERROR: Datos inválidos' + req.body });
 
     const motherFound = await Mother.findOne({ rut });
 
     //se valida la existencia de la madre en el sistema
     if ( motherFound )
-        return res.status(301).send({ succes: false, data:{}, message:"ERROR: La madre ya está registrada en el sistema." });
+        return res.status(301).send({ succes: false, data:{}, message:'ERROR: La madre ya está registrada en el sistema.' });
     
     const newMother = {
         name, rut, commune, phone_number, mail, birth, 
@@ -37,7 +36,7 @@ export const newMother: RequestHandler = async (req, res) => {
     const motherSaved = new Mother(newMother);
     await motherSaved.save();
 
-    return res.status(201).send({ succes: true, data: { _id: motherSaved._id }, message: "Madre agregada con éxito al sistema." });
+    return res.status(201).send({ succes: true, data: { _id: motherSaved._id }, message: 'Madre agregada con éxito al sistema.' });
 }
 
 export const editMother: RequestHandler = async (req, res) => {
