8bb9d8bb27c942bb36aefd437612295de54dc4e0
DanRivaille <dan.santos.hd@outlook.com>
2021-11-14 18:43:53 -0300

    Se prueban trayendo algunos datos desde el backend

--- Front-end/lactaProject/src/app/Modules/mothers/components/mother-data/mother-data.component.ts
+++ Front-end/lactaProject/src/app/Modules/mothers/components/mother-data/mother-data.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { typeMother } from '@interfaces/mother';
+import { ApiResponse } from '@interfaces/api_response';
+import { ApiGetService } from 'src/app/services/api-get.service';
 
 @Component({
   selector: 'app-mother-data',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MotherDataComponent implements OnInit {
 
-  constructor() { }
+  public motherData: typeMother;
+
+  constructor(private apiGet: ApiGetService) { }
 
   ngOnInit(): void {
+    this.apiGet.getMother('618b49b310ac658e96ca116d').subscribe((response: ApiResponse) => {
+      console.log(response);
+    });
   }
-
 }
