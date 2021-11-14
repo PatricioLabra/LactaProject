c441fad68a67cca18a50c5cbdbdd0492c627d4c0
Liiinkay <diegofaraya1@hotmail.com>
2021-11-14 19:31:57 -0300

    Se realizaron los fixes, tambien se arreglo un tema con el select

--- Front-end/lactaProject/src/app/Modules/professionals/pages/professional-form/professional-form.component.ts
+++ Front-end/lactaProject/src/app/Modules/professionals/pages/professional-form/professional-form.component.ts
@@ -11,18 +11,18 @@ export class ProfessionalFormComponent implements OnInit {
   constructor(private fb:FormBuilder) {
     this.form=this.fb.group({
       name: ['', Validators.required],
-      rut: ['',Validators.required],
-      rut_vc: ['',Validators.required],
-      password: ['',Validators.required],
-      mail: ['',Validators.email],
-      permission_level: ['',Validators.required]
+      rut: ['', Validators.required],
+      rut_vc: ['', Validators.required],
+      password: ['', Validators.required],
+      mail: ['', [Validators.email, Validators.required]],
+      permission_level: ['0', Validators.required]
     });
    }
   ngOnInit(): void {
   }
 
   // Funcion que ACTUALMENTE solo se encarga de imprimir por consola los valores obtenidos en el formulario
-  send_professional_data(){
+  sendProfessionalData(){
     console.log(this.form.get("name")?.value);
     console.log(this.form.get("rut")?.value + "-" + this.form.get("rut_vc")?.value);
     console.log(this.form.get("password")?.value);
