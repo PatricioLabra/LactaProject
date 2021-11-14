e42344560790a38a0a21bc81677e3b84efe0dd2d
DanRivaille <dan.santos.hd@outlook.com>
2021-11-14 18:31:11 -0300

    Se agrega la funcion al get-service para obtener los datos de una asesorada

--- Front-end/lactaProject/src/app/services/api-get.service.ts
+++ Front-end/lactaProject/src/app/services/api-get.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { ApiClass } from './api.class';
+import { ApiResponse } from '@interfaces/api_response';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,14 @@ export class ApiGetService extends ApiClass {
 
   constructor(http: HttpClient) {
     super(http);
+  }
+
+  /**
+   * Obtiene la informacion de una asesorada en particular, dependiendo del id ingresado
+   * @param idMother Id de la asesorada a obtener los datos
+   */
+  public getMother(idMother: string): Observable<ApiResponse> {
+    const url: string = this.makeUrl(['mother', idMother]);
+    return this.http.get<ApiResponse>(url);
   }
 }
