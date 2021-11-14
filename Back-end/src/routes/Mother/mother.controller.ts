9a1aa5f2ad675c40e636e28963ad2648a2e1b127
PatricioLabra <patricio.labra827@gmail.com>
2021-11-14 17:05:02 -0300

    se le agrega un encabezado a la funcion destructureMother

--- Back-end/src/routes/Mother/mother.controller.ts
+++ Back-end/src/routes/Mother/mother.controller.ts
@@ -81,6 +81,12 @@ export const getMothers: RequestHandler = async (req, res) => {
 export const getSearch: RequestHandler = async (req, res) => {
     
 }
+
+/**
+ * Extrae los atributos del perfil de la madre extraidos desde la base de datos
+ * @param motherFound Madre extraido de la base de datos
+ * @returns Object con los atributos de la madre a enviar al front
+ */
 function destructureMother(motherFound: any) {
     const motherFiltered = {
         _id: motherFound._id,
