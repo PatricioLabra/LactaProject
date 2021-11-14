dea5d2ff1aa1b6c17e299dc661d5f621cfbc34bf
DanRivaille <dan.santos.hd@outlook.com>
2021-11-14 18:49:11 -0300

    Se agrega Quicksand a styles.scss para que este disponible globalmente en el proyecto

--- Front-end/lactaProject/src/app/interfaces/mother.ts
+++ Front-end/lactaProject/src/app/interfaces/mother.ts
@@ -3,13 +3,13 @@ export interface typeMother{
     name: string,
     rut: string,
     commune: string,
-    phone_number:string,
-    mail:string,
-    birth:Date,
-    ocupation:string,
-    studies:string,
-    marital_status:string,
-    forecast:string,
-    chronic_deseases:string[],
-    number_of_living_children:Number
+    phone_number: string,
+    mail: string,
+    birth: Date,
+    ocupation: string,
+    studies: string,
+    marital_status: string,
+    forecast: string,
+    chronic_deseases: string[],
+    number_of_living_children: Number
 }
