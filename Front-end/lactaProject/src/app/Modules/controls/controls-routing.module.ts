bf96934323fd077d0b30bceadc57a5e0c288f18b
Demitoo <76886434+Demitoo@users.noreply.github.com>
2021-11-13 22:31:18 -0300

    Componente pop-up imprimir cita
    pequeño componente pa despues meter al popUp

--- Front-end/lactaProject/src/app/Modules/controls/controls-routing.module.ts
+++ Front-end/lactaProject/src/app/Modules/controls/controls-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { PopUpPrintControlComponent } from './components/pop-up-print-control/pop-up-print-control.component';
 
 
 
-const routes: Routes=[];
+const routes: Routes=[
+  {path:"prueba",component:PopUpPrintControlComponent}
+];
 
 @NgModule({
   declarations: [],
