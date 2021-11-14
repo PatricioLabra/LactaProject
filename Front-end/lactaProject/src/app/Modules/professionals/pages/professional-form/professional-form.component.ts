525aba4c4241e118081c46b9206b74924cb157ba
Liiinkay <diegofaraya1@hotmail.com>
2021-11-14 07:15:39 -0300

    Se realiza el formulario de profesionales

--- Front-end/lactaProject/src/app/Modules/professionals/pages/professional-form/professional-form.component.ts
+++ Front-end/lactaProject/src/app/Modules/professionals/pages/professional-form/professional-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-professional-form',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./professional-form.component.scss']
 })
 export class ProfessionalFormComponent implements OnInit {
-
-  constructor() { }
-
+  form:FormGroup;
+  constructor(private fb:FormBuilder) {
+    this.form=this.fb.group({
+      name: ['', Validators.required],
+      rut: ['',Validators.required],
+      rut_vc: ['',Validators.required],
+      password: ['',Validators.required],
+      mail: ['',Validators.email],
+      permission_level: ['',Validators.required]
+    });
+   }
   ngOnInit(): void {
   }
 
+  // Funcion que ACTUALMENTE solo se encarga de imprimir por consola los valores obtenidos en el formulario
+  send_professional_data(){
+    console.log(this.form.get("name")?.value);
+    console.log(this.form.get("rut")?.value + "-" + this.form.get("rut_vc")?.value);
+    console.log(this.form.get("password")?.value);
+    console.log(this.form.get("mail")?.value);
+    console.log(this.form.get("permission_level")?.value);
+  }
 }
