0daa7c0fc123d6ca06be176f10a4d489544ed675
PatricioLabra <patricio.labra827@gmail.com>
2021-11-16 16:17:27 -0300

    se agregan las válidaciones de newControl

--- Back-end/src/routes/Control/control.controller.ts
+++ Back-end/src/routes/Control/control.controller.ts
@@ -1,6 +1,30 @@
 import { RequestHandler } from "express";
+import { Types } from "mongoose";
 
+
+/**
+ * Función que maneja la petición de agregar a un nuevo control al sistema.
+ * @route Post /control/
+ * @param req Request de la petición, se espera que tenga la información del nuevo control + el id Del child
+ * @param res Response, retorna un un object con success:true, data:{ _id: ObjectId() } y un message: "String" del nuevo control si todo sale bien
+ */
 export const newControl: RequestHandler = async (req, res) => {
+    const { id_child, newControl } = req.body;
+
+    //se valida el Id del Child
+    if ( !Types.ObjectId.isValid(id_child) )
+        return res.status(400).send({ success: false, data:{}, message: 'ERROR: El id ingresado no es válido.' });
+
+    const childFound = await Child.findById(id_child);
+
+    //se valida la existencia del Child
+    if ( !childFound )
+        return res.status(404).send({ success: false, data:{}, message: 'ERROR: El lactante ingresado no existe en el sistema.' });
+
+    //se validan los campos required del control
+    if ( !newControl.child_name || !newControl.consultation_place || !newControl.monitoring_medium || !newControl.date_control )
+        return res.status(400).send({ success: false, data:{}, message: 'ERROR: Los datos del control no son válidos.' + req.body });
+
 
 }
 
