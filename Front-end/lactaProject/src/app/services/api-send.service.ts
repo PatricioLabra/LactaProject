07699305d5ebd937e61aa58a0ac4c9cb108ca7c8
DanRivaille <dan.santos.hd@outlook.com>
2021-11-17 15:16:25 -0300

    Se agrega la funcion para agregar una madra al sistema

--- Front-end/lactaProject/src/app/services/api-send.service.ts
+++ Front-end/lactaProject/src/app/services/api-send.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CoreModule } from '@core/core.module';
 import { ApiClass } from './api.class';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '@interfaces/api_response';
+import { typeMother } from '@interfaces/mother';
 
 @Injectable({
   providedIn: CoreModule
@@ -10,5 +13,14 @@ export class ApiSendService extends ApiClass {
 
   constructor(http: HttpClient) {
     super(http);
+  }
+
+  /**
+   * Agrega una nueva madra al sistema
+   * @param motherInfo Informacion de la nueva madra a agregar
+   */
+  public addMother(motherInfo: typeMother): Observable<ApiResponse> {
+    const url: string = this.makeUrl(['mother']);
+    return this.http.post<ApiResponse>(url, motherInfo);
   }
 }
