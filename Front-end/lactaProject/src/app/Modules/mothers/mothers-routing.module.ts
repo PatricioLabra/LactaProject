2ec0810b95761586624645176c25467cba168d09
Demitoo <76886434+Demitoo@users.noreply.github.com>
2021-11-17 20:38:09 -0300

    conexion entre agregar profesional,asesorada y formularios. Tambien el elimianr profesional esta funcional

--- Front-end/lactaProject/src/app/Modules/mothers/mothers-routing.module.ts
+++ Front-end/lactaProject/src/app/Modules/mothers/mothers-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { MothersViewComponent } from './pages/mothers-view/mothers-view.component';
+import { MotherFormComponent } from './pages/mother-form/mother-form.component';
 
 
 
 const routes: Routes=[
-  {path:"asesoradas", component:MothersViewComponent}
+  {path:"asesoradas", component:MothersViewComponent},
+  {path:"asesoradas/agregar",component:MotherFormComponent}
 ];
 
 @NgModule({
