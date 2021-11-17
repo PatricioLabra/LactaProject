2b23187cb745e4e7a3a142711ad61becbef966ae
Liiinkay <diegofaraya1@hotmail.com>
2021-11-17 08:02:43 -0300

    Primera version de los form de citas en proceso

--- Front-end/lactaProject/src/app/Modules/controls/controls-routing.module.ts
+++ Front-end/lactaProject/src/app/Modules/controls/controls-routing.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { PastControlsComponent } from './pages/past-controls/past-controls.component';
 import { FirstControlFormComponent } from './pages/first-control-form/first-control-form.component';
-
+import { ControlFormComponent } from './pages/control-form/control-form.component';
 
 const routes: Routes=[
   {path:"prueba",component:PastControlsComponent},
-  {path: "prueba2", component:FirstControlFormComponent}
+  {path: "prueba2", component:FirstControlFormComponent},
+  {path: "prueba3", component:ControlFormComponent}
 ];
 
 @NgModule({
--- Front-end/lactaProject/src/app/Modules/controls/pages/control-form/control-form.component.ts
+++ Front-end/lactaProject/src/app/Modules/controls/pages/control-form/control-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-control-form',
@@ -6,10 +7,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./control-form.component.scss']
 })
 export class ControlFormComponent implements OnInit {
-
-  constructor() { }
-
+  indications:Array<string>=[];
+  form:FormGroup;
+  constructor(private fb:FormBuilder) {
+    this.form=this.fb.group({
+      consultation_place: ['domicilio'],
+      monitoring_medium: ['whatsapp'],
+      date_control: ['', Validators.required],
+      age: ['', Validators.required],
+      weight: ['', Validators.required],
+      reason_of_consultation: ['', Validators.required],
+      other_consultation: [''],
+      accompanied_by: ['', Validators.required],
+      other_companion: [''],
+      emotional_status: ['', Validators.required],
+      observations: ['', Validators.required],
+      mejorar_acople: [''],
+      a_frecuencia: [''],
+      pezonera: [''],
+      sonda_al_dedo: [''],
+      relactacion: [''],
+      next_control: ['false'],
+      consultation_place2: [''],
+      monitoring_medium2: [''],
+      date_control2: ['']
+    });
+   }
   ngOnInit(): void {
   }
+  // Funcion que imprime por consola los form control value
+  sendControlData(){
+    this.createList;
+    console.log(this.form.get("consultation_place")?.value);
+    console.log(this.form.get("monitoring_medium")?.value);
+    console.log(this.form.get("date_control")?.value);
+    console.log(this.form.get("age")?.value);
+    console.log(this.form.get("weight")?.value);
+    console.log(this.form.get("reason_of_consultation")?.value);
+    console.log(this.form.get("other_consultation")?.value);
+    console.log(this.form.get("accompanied_by")?.value);
+    console.log(this.form.get("other_companion")?.value);
+    console.log(this.form.get("emotional_status")?.value);
+    console.log(this.form.get("observations")?.value);
+    console.log(this.indications);
+    console.log(this.form.get("next_control")?.value);
+    console.log(this.form.get("consultation_place2")?.value);
+    console.log(this.form.get("monitoring_medium2")?.value);
+    console.log(this.form.get("date_control2")?.value);
+  }
 
+  // Funcion que crea una lista de enfermedades cronicas
+  createList(){
+    if(this.form.get("mejorar_acople")?.value == true){
+      this.indications.push("mejorar acople"); 
+    }
+    if(this.form.get("a_frecuencia")?.value == true){
+      this.indications.push("aumento de frecuencia al pecho/extractor"); 
+    }
+    if(this.form.get("pezonera")?.value == true){
+      this.indications.push("pezonera"); 
+    }
+    if(this.form.get("sonda_al_dedo")?.value == true){
+      this.indications.push("sonda al dedo"); 
+    }
+    if(this.form.get("relactacion")?.value == true){
+      this.indications.push("relactación"); 
+    }
+    if(this.indications.length == 0){
+      this.indications.push("ninguna");
+    }
+  }
 }
