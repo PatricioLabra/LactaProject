b7d5daf7871c11e3012a7510ce32cc93bd58105d
Demitoo <76886434+Demitoo@users.noreply.github.com>
2021-11-17 17:53:24 -0300

    Controles pasados y popup sacando datos desde back-end

--- Front-end/lactaProject/src/app/Modules/controls/components/control-data/control-data.component.ts
+++ Front-end/lactaProject/src/app/Modules/controls/components/control-data/control-data.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
+import { ApiResponse } from '@interfaces/api_response';
+import { typeControl } from '@interfaces/control';
+import { ApiGetService } from 'src/app/services/api-get.service';
 
 @Component({
   selector: 'app-control-data',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ControlDataComponent implements OnInit {
 
-  constructor() { }
+  @Input() controlId:string;
+  controlData=0;
+
+  
+  constructor( private apiGet: ApiGetService) {}
 
   ngOnInit(): void {
+    this.apiGet.getControl(this.controlId).subscribe((response: ApiResponse) => {
+      console.log(response);
+      if (response.success) {
+        this.controlData=response.data;
+        console.log(this.controlData);
+      }
+    });
   }
 
 }
