239f29b811aac2facee2ba1d830382a761448d4a
Liiinkay <diegofaraya1@hotmail.com>
2021-11-17 15:45:36 -0300

    Se agrega el campo de Nombre para el child forms y se arreglan conflictos

--- Front-end/lactaProject/src/app/Modules/controls/controls.module.ts
+++ Front-end/lactaProject/src/app/Modules/controls/controls.module.ts
@@ -1,30 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 import { PastControlsComponent } from './pages/past-controls/past-controls.component';
 import { InprocessControlComponent } from './pages/inprocess-control/inprocess-control.component';
 import { MotherIncomingcontrolsListComponent } from './components/mother-incomingcontrols-list/mother-incomingcontrols-list.component';
 import { ControlDataComponent } from './components/control-data/control-data.component';
-import { AddControlComponent } from './components/add-control/add-control.component';
 import { ControlsListComponent } from './components/controls-list/controls-list.component';
 import { ControlsRoutingModule } from './controls-routing.module';
 import { PopUpPrintControlComponent } from './components/pop-up-print-control/pop-up-print-control.component';
-import { SharedModule } from '@shared/shared.module';
+import { PopupScheduleControlComponent } from './components/popup-schedule-control/popup-schedule-control.component';
 import { PastControlsListComponent } from './components/past-controls-list/past-controls-list.component';
 import { FirstControlFormComponent } from './pages/first-control-form/first-control-form.component';
 import { ControlFormComponent } from './pages/control-form/control-form.component';
 
 
-
 @NgModule({
   declarations: [
-    PastControlsComponent, 
-    InprocessControlComponent, 
-    MotherIncomingcontrolsListComponent, 
-    ControlDataComponent, 
-    AddControlComponent, 
-    ControlsListComponent, 
-    PopUpPrintControlComponent, 
-    PastControlsListComponent, 
+    PastControlsComponent,
+    InprocessControlComponent,
+    MotherIncomingcontrolsListComponent,
+    ControlDataComponent,
+    ControlsListComponent,
+    PopUpPrintControlComponent,
+    PopupScheduleControlComponent,
+    PastControlsListComponent,
     FirstControlFormComponent, 
     ControlFormComponent
   ],
@@ -34,14 +33,12 @@ import { ControlFormComponent } from './pages/control-form/control-form.componen
     SharedModule
   ],
   exports:[
-    PastControlsComponent, 
-    InprocessControlComponent, 
-    MotherIncomingcontrolsListComponent, 
-    ControlDataComponent, 
-    AddControlComponent, 
+    PastControlsComponent,
+    InprocessControlComponent,
+    MotherIncomingcontrolsListComponent,
+    PopupScheduleControlComponent,
+    ControlDataComponent,
     ControlsListComponent,
-    PopUpPrintControlComponent, 
-    PastControlsListComponent, 
     FirstControlFormComponent, 
     ControlFormComponent
   ]
