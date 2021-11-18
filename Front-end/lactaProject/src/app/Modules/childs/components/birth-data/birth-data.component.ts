694923f3d1f4c88ef447e7c7d7684ed7b5393d1c
DanRivaille <dan.santos.hd@outlook.com>
2021-11-18 00:56:09 -0300

    Se arregla un drama

--- Front-end/lactaProject/src/app/Modules/childs/components/birth-data/birth-data.component.ts
+++ Front-end/lactaProject/src/app/Modules/childs/components/birth-data/birth-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ApiResponse } from '@interfaces/api_response';
 import { typeChild } from '@interfaces/child';
 import { ApiGetService } from 'src/app/services/api-get.service';
@@ -10,13 +10,12 @@ import { ApiGetService } from 'src/app/services/api-get.service';
 })
 export class BirthDataComponent implements OnInit {
 
-  childId="61955ea11b759d88b0b06a1d" //Para conectar Solo hay q asignar este id desde la ruta
-  childData:typeChild;
-
+  @Input()
+  public childId: string;
+  public childData:typeChild;
 
   ngOnInit() {
     this.apiGet.getChild(this.childId).subscribe((response: ApiResponse) => {
-      console.log(response);
       if (response.success) {
         this.childData=response.data
       }
