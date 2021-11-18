3b82085ef588487fa65be38ab474c62f2ee7390a
Liiinkay <diegofaraya1@hotmail.com>
2021-11-18 18:14:03 -0300

    Editar cita version1, ahora funciona con la base de datos

--- Front-end/lactaProject/src/app/Modules/mothers/components/controls-list/controls-list.component.ts
+++ Front-end/lactaProject/src/app/Modules/mothers/components/controls-list/controls-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ApiResponse } from '@interfaces/api_response';
 import { typeControl } from '@interfaces/control';
 import { ApiGetService } from 'src/app/services/api-get.service';
@@ -19,6 +19,8 @@ export class ControlsListComponent implements OnInit {
   actualControlId:string;
   public controls: typeControl[] | null = null;
   public controlForm: FormGroup;
+  public element:any;
+  controlId="";
 
   constructor(private apiGet: ApiGetService , private apiSend:ApiSendService, private router:Router, private fb:FormBuilder) { }
 
@@ -38,6 +40,7 @@ export class ControlsListComponent implements OnInit {
       monitoring_medium: ['ninguna', Validators.required],
       date_control: [null, Validators.required]
     });
+    
   }
 
   show(value:string){
@@ -56,8 +59,36 @@ export class ControlsListComponent implements OnInit {
     this.controls=this.controls.filter((controls)=>controls._id!=value);
   }
 
+  editarCita(){
+    this.element.child_name = this.controlForm.get('name_child')?.value;
+    this.element.consultation_place = this.controlForm.get('consultation_place')?.value;
+    this.element.monitoring_medium = this.controlForm.get('monitoring_medium')?.value;
+    this.element.date_control = this.controlForm.get('date_control')?.value;
+    this.apiSend.updateControl(this.element).subscribe((response:ApiResponse)=>{
+      console.log(response);
+    });
+  }
+
   show2(value:string){
-    this.actualControlId=value;
-    this.frameModal2.show();
+    this.apiGet.getControl(value).subscribe((response: ApiResponse) => {
+      if (response.success) {
+        this.element = response.data;
+        console.log(this.element);
+        console.log(response);
+        console.log(value);
+        this.fillInputs();
+        this.actualControlId=value;
+        this.frameModal2.show();
+      }
+    });
+  
+  }
+  
+  // Funcion que rellena los datos de la asesorada en los form control
+  fillInputs(){
+    this.controlForm.get('name_child')?.setValue(this.element.child_name);
+    this.controlForm.get('consultation_place')?.setValue(this.element.consultation_place);
+    this.controlForm.get('monitoring_medium')?.setValue(this.element.monitoring_medium);
+    this.controlForm.get('date_control')?.setValue(this.element.date_control);
   }
 }
