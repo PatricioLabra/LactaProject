d3fccbf2d6dc9baf8f71ce9cd31a020ccacf6d2d
Demitoo <76886434+Demitoo@users.noreply.github.com>
2021-11-17 21:38:34 -0300

    eliminar lactante funcionando

--- Front-end/lactaProject/src/app/Modules/childs/pages/child-profile/child-profile.component.ts
+++ Front-end/lactaProject/src/app/Modules/childs/pages/child-profile/child-profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiResponse } from '@interfaces/api_response';
 import { typeChild } from '@interfaces/child';
 import { ApiGetService } from 'src/app/services/api-get.service';
+import { ApiSendService } from 'src/app/services/api-send.service';
 
 @Component({
   selector: 'app-child-profile',
@@ -12,6 +13,7 @@ export class ChildProfileComponent implements OnInit {
 
   childId="61955ea11b759d88b0b06a1d" //Para conectar Solo hay q asignar este id desde la ruta
   childData:typeChild;
+  @ViewChild('frame', { static: true }) public frameModal;
 
 
   ngOnInit() {
@@ -24,10 +26,20 @@ export class ChildProfileComponent implements OnInit {
     });
 
   }
-  constructor( private apiGet: ApiGetService) {}
+  constructor( private apiGet: ApiGetService , private apiSend:ApiSendService) {}
 
-  eliminarLactante(){
-    console.log("XD")
+  eliminarLactante(value:string){
+    this.apiSend.deleteChild(value).subscribe( (response:ApiResponse) =>{
+      if(response.success){
+        console.log("lactante eliminado");
+      }
+      this.frameModal.hide();
+    })
+  }
+  show(){
+    
+    this.frameModal.show();
+
   }
 
 }
