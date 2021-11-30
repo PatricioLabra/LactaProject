6c07acb77d8351b5f4ff9537d15a9e0ef3f01474
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-11-30 02:13:19 -0300

    se agregaron varias cosas pero quedaron funcionando los filtros que envian informacion al front para graficar :v

--- Back-end/src/routes/Graphic/graphic.routes.ts
+++ Back-end/src/routes/Graphic/graphic.routes.ts
@@ -4,6 +4,6 @@ import * as graphicCtrl from './graphic.controller';
 const router = Router();
 
 //Obtener objeto con los datos a graficar
-router.get('/graphic/:keyword',graphicCtrl.getDataGraphic);
+router.get('/graphic/:keyword', graphicCtrl.getDataGraphic);
 
 export default router;
