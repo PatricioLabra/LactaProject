222d28c33421deb2a935579105fc802f9c5338cd
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2021-12-01 03:19:27 -0300

    se agrega los verifyToken en la ruta de controles

--- Back-end/src/routes/Control/control.routes.ts
+++ Back-end/src/routes/Control/control.routes.ts
@@ -5,28 +5,28 @@ import { verifyToken } from '../jwt';
 const router = Router();
 
 //agregar nuevo control
-router.post('/control', controlCtrl.newControl);
+router.post('/control', verifyToken, controlCtrl.newControl);
 
 //modificar control
-router.put('/control/:idControl', controlCtrl.editControl);
+router.put('/control/:idControl', verifyToken, controlCtrl.editControl);
 
 //eliminar control
-router.delete('/control/:idControl', controlCtrl.deleteControl);
+router.delete('/control/:idControl', verifyToken, controlCtrl.deleteControl);
 
 //obtener lista controles pendientes resumidos por madre(id)
-router.get('/control/:idMother', controlCtrl.getNextControls);
+router.get('/control/:idMother', verifyToken, controlCtrl.getNextControls);
 
 //obtener controles pasados resumido por madre(id)
-router.get('/control/past/:idMother', controlCtrl.getPassControls);
+router.get('/control/past/:idMother', verifyToken, controlCtrl.getPassControls);
 
 //obtener informacion control pasado detallado(idControlPasado_)
-router.get('/control/profile/:idControl', controlCtrl.getDetailedPassControl);
+router.get('/control/profile/:idControl', verifyToken, controlCtrl.getDetailedPassControl);
 
 // Obtener lista controles filtrada por nombre y fechas
-router.get('/controlsFiltered', controlCtrl.getSearchControlFiltered);
+router.get('/controlsFiltered', verifyToken, controlCtrl.getSearchControlFiltered);
 
 //Obtener el ultimo y próximo control asociado a una madre
-router.get('/control/lastAndNext/:idMother', controlCtrl.getLastAndNextControl);
+router.get('/control/lastAndNext/:idMother', verifyToken, controlCtrl.getLastAndNextControl);
 
 //Obtener cantidad controles de 1 lactante
 router.get('/control/quantity/:idChild',controlCtrl.getFirstControl);
