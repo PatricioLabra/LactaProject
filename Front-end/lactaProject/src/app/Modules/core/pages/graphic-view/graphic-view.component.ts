f29fd686584d7fd787da9bd5d4e07853ec902a00
DanRivaille <dan.santos.hd@outlook.com>
2021-12-01 21:19:08 -0300

    Se agregan las interfaces de los graficos

--- Front-end/lactaProject/src/app/Modules/core/pages/graphic-view/graphic-view.component.ts
+++ Front-end/lactaProject/src/app/Modules/core/pages/graphic-view/graphic-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiResponse } from '@interfaces/api_response';
+import { GraphicData } from '@interfaces/graphic';
+import { ApiGetService } from 'src/app/services/api-get.service';
 
 @Component({
   selector: 'app-graphic-view',
@@ -9,11 +12,17 @@ import { ActivatedRoute } from '@angular/router';
 export class GraphicViewComponent implements OnInit {
 
   public keyword: string = null;
+  public dataGraphic: GraphicData;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private apiGet: ApiGetService) { }
 
   ngOnInit(): void {
     this.keyword = this.activatedRoute.snapshot.params.keyword;
-  }
 
+    this.apiGet.getGraphicData(this.keyword).subscribe((response: ApiResponse) => {
+      if (response.success) {
+        this.dataGraphic = response.data;
+      }
+    });
+  }
 }
