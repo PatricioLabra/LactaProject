a81efe06e1e316514ad9e7d5be9e9ff379c5ada8
Demitoo <76886434+Demitoo@users.noreply.github.com>
2021-12-02 01:54:44 -0300

    cambios

--- Front-end/lactaProject/src/app/services/can-activate-admin.service.ts
+++ Front-end/lactaProject/src/app/services/can-activate-admin.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { UserInfoService } from './user-info.service';
 
 @Injectable()
 export class CanActivateAdminService implements CanActivate{
-  constructor(private router:Router) { }
+  constructor(private router:Router, private userService:UserInfoService) { }
 
-  isLogged=1
+  isLogged;
+  rol;
   canActivate(){
-    if(this.isLogged==null){
+    this.userService.getUserInfo.subscribe((response:any)=>{
+      this.rol=response.role;
+    });
+    this.userService.getIsLoggedin.subscribe((response:boolean)=>{
+      this.isLogged=response;
+    });
+    if(this.isLogged!=true){
       console.log("No estas logeado");
       this.router.navigate([""])
       return false;
     }
-    if(this.isLogged!=1){
+    if(this.rol!=1){
       console.log("No estas logeado como admin");
       this.router.navigate([""])
       return false;
