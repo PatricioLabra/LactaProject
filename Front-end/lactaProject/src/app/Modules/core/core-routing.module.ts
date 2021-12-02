88730ec1cd51643d23a64ba58a4528bf1d4438a3
DanRivaille <dan.santos.hd@outlook.com>
2021-12-01 21:06:12 -0300

    Se agrega la ruta de los graficos

--- Front-end/lactaProject/src/app/Modules/core/core-routing.module.ts
+++ Front-end/lactaProject/src/app/Modules/core/core-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { ControlPanelComponent } from './pages/control-panel/control-panel.component';
 import { CanActivateService } from 'src/app/services/can-activate.service';
 import { StadisticsPanelComponent } from './pages/stadistics-panel/stadistics-panel.component';
+import { GraphicViewComponent } from './pages/graphic-view/graphic-view.component';
 
 const routes: Routes=[
-  { path: "control-panel", component: ControlPanelComponent , canActivate:[CanActivateService]},
-  { path: "panel-estadisticas",component:StadisticsPanelComponent, canActivate:[CanActivateService]}
+  { path: "control-panel", component: ControlPanelComponent , canActivate: [CanActivateService] },
+  { path: "panel-estadisticas", component: StadisticsPanelComponent, canActivate: [CanActivateService] },
+  { path: "graphic/:keyword", component: GraphicViewComponent, canActivate: [CanActivateService] }
 ];
 
 @NgModule({
