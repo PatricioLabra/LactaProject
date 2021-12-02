5fbb7bc9be8ad96db759e9dbf0fe116f512e4e37
Demitoo <76886434+Demitoo@users.noreply.github.com>
2021-12-01 23:57:25 -0300

    GRAFICOS LISTOS

--- Front-end/lactaProject/src/app/Modules/core/pages/graphic-view/graphic-view.component.ts
+++ Front-end/lactaProject/src/app/Modules/core/pages/graphic-view/graphic-view.component.ts
@@ -13,6 +13,18 @@ export class GraphicViewComponent implements OnInit {
 
   public keyword: string = null;
   public dataGraphic: GraphicData;
+  public chartType: string = 'bar';
+  public flag=0
+
+  public chartDatasets: Array<any> = [
+    { 
+      data: [],
+      label: '', 
+    }
+  ];
+  public dataux:Array<any>=[]
+  public labelaux:Array<any>=[]
+  public chartLabels: Array<any> = [];
 
   constructor(private activatedRoute: ActivatedRoute, private apiGet: ApiGetService) { }
 
@@ -21,8 +33,58 @@ export class GraphicViewComponent implements OnInit {
 
     this.apiGet.getGraphicData(this.keyword).subscribe((response: ApiResponse) => {
       if (response.success) {
+        
         this.dataGraphic = response.data.options;
+        this.chartDatasets[0].label=response.data.name_data + " Chart";
+        for(var i in this.dataGraphic){
+          this.dataux.push(this.dataGraphic[i].value)
+          this.chartLabels.push(this.dataGraphic[i].name)
+        }
+        this.dataux.push(Math.max.apply(null,this.dataux)*1.3)
+        this.chartDatasets[0].data=this.dataux;
+        this.flag=1;
       }
     });
   }
+
+
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 2,
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          stepSize:1,
+        }
+      }]
+    }
+  
+  };
+
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
 }
+
