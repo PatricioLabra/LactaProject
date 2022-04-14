34fc289d3b60484775098ec3f7f6d67ab0b691d1
Patricio Labra <patricio.labra827@gmail.com>
2022-04-14 14:38:52 -0400

    se comenta la url de producción

--- Front-end/lactaProject/src/environments/environment.prod.ts
+++ Front-end/lactaProject/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:4000'
+  API_URL: 'http://localhost:4000' //producción: https://compact-citizen-346817.rj.r.appspot.com
 };
--- Front-end/lactaProject/src/environments/environment.ts
+++ Front-end/lactaProject/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:4000'
+  API_URL: 'http://localhost:4000' //producción: https://compact-citizen-346817.rj.r.appspot.com
 };
 
 /*
