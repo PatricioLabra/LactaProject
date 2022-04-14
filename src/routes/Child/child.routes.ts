59d4a113d9493ed721c2bf603751d56d47a2890a
Patricio Labra <patricio.labra827@gmail.com>
2022-04-14 13:37:42 -0400

    se saca el passport de graphic y se redireccionan las toutes a /

--- src/routes/Child/child.routes.ts
+++ src/routes/Child/child.routes.ts
@@ -6,18 +6,18 @@ import passport from 'passport';
 const router = Router();
 
 //agregar nuevo lactante
-router.post('/child/:idMother', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol, childCtrl.newChild);
+router.post('/child/:idMother', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol, childCtrl.newChild);
 
 //modificar Lactantee
-router.put('/child/:idLactante', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol, childCtrl.editChild);
+router.put('/child/:idLactante', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol, childCtrl.editChild);
 
 //eliminar Lactante en la lista de la madre
-router.delete('/child/:idLactante', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol, childCtrl.deleteChild);
+router.delete('/child/:idLactante', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol, childCtrl.deleteChild);
 
 //obtener lista Lactante resumido(idMother)
-router.get('/child/:idMother', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol, childCtrl.getResumeChild);
+router.get('/child/:idMother', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol, childCtrl.getResumeChild);
 
 //obtener Lactante completo
-router.get('/child/profile/:idLactante', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol, childCtrl.getChild);
+router.get('/child/profile/:idLactante', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol, childCtrl.getChild);
 
 export default router
--- src/routes/Mother/mother.routes.ts
+++ src/routes/Mother/mother.routes.ts
@@ -6,18 +6,18 @@ import passport from 'passport';
 const router = Router();
 
 //agregar nueva asesorada 
-router.post('/mother', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol, motherCtrl.newMother);
+router.post('/mother', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol, motherCtrl.newMother);
 
 //modificar una asesorada
-router.put('/mother/:id', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol, motherCtrl.editMother);
+router.put('/mother/:id', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol, motherCtrl.editMother);
 
 //eliminar una asesorada
-router.delete('/mother/:id', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol, motherCtrl.deleteMother);
+router.delete('/mother/:id', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol, motherCtrl.deleteMother);
 
 //obtener asesorada detallada (panel completo)
-router.get('/mother/:id', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol, motherCtrl.getDetailedMother);
+router.get('/mother/:id', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol, motherCtrl.getDetailedMother);
 
 //obtener todas las asesoradas
-router.get('/mother', passport.authenticate('jwt', {session: false, failureRedirect: '/login' }), isRol,  motherCtrl.getMothers);
+router.get('/mother', passport.authenticate('jwt', {session: false, failureRedirect: '/' }), isRol,  motherCtrl.getMothers);
 
 export default router;
