0804fe2e5a629df8cd97cadf6410964c463ea7c0
Cesar Gordon Muena <82060989+CesarGordon@users.noreply.github.com>
2022-08-15 19:41:03 -0400

    se modifican los required del archivo child.model

--- Back-end/src/routes/Child/child.model.ts
+++ Back-end/src/routes/Child/child.model.ts
@@ -7,28 +7,28 @@ const childSchema = new Schema({
     trim: true
   },
   gestacion_data:{
-    diseases_during_pregnancy:{ required:true, type: [String] } ,
-    planned_pregnancy: { required:true, type: Boolean },
-    assisted_fertilization: { required: true, type: Boolean },
-    previous_lactaction: { required: true, type: String },
-    duration_of_past_lactaction_in_months: { required: true, type: Number, min: 0 },
-    breastfeeding_education: { required: true, type: Boolean }
+    diseases_during_pregnancy:{ required:false, type: [String] } ,
+    planned_pregnancy: { required:false, type: Boolean },
+    assisted_fertilization: { required: false, type: Boolean },
+    previous_lactaction: { required: false, type: String },
+    duration_of_past_lactaction_in_months: { required: false, type: Number, min: 0 },
+    breastfeeding_education: { required: false, type: Boolean }
   },
   birth_data:{
     birthplace: { required:true, type: String },
     type_of_birth: { required:true, type: String },
     birthday: { required:true, type: Date },
-    gestional_age: { required:true, type: Number },
-    gender: { required:true, type: String },
-    birth_weight: { required:true, type: Number },
-    skin_to_skin_contact:{ required:true, type: Boolean },
-    breastfeeding_b4_2hours:{ required:true, type: Boolean },
-    has_suplement:{ required: true, type: Boolean },
+    gestional_age: { required:false, type: Number },
+    gender: { required:false, type: String },
+    birth_weight: { required:false, type: Number },
+    skin_to_skin_contact:{ required:false, type: Boolean },
+    breastfeeding_b4_2hours:{ required:false, type: Boolean },
+    has_suplement:{ required: false, type: Boolean },
     why_recived_suplement:{ required:false, type: String },
-    joint_accommodation:{ required:true, type: Boolean },
-    use_of_pacifier:{ required:true, type: Boolean },
-    post_discharge_feeding:{ required:true, type: String },
-    last_weight_control:{ required:true, type: Number }
+    joint_accommodation:{ required:false, type: Boolean },
+    use_of_pacifier:{ required:false, type: Boolean },
+    post_discharge_feeding:{ required:false, type: String },
+    last_weight_control:{ required:false, type: Number }
   },
   id_mother:{ required:true, type: String }
 },{
