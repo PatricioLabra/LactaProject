fc0a9bbde2bf910fcaf1eef66ec5a3394a3881e9
Patricio Labra <patricio.labra827@gmail.com>
2022-09-22 17:58:02 -0300

    se actualiza el environment a producción

--- Front-end/lactaProject/src/environments/environment.prod.ts
+++ Front-end/lactaProject/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:4000/api' //producción: https://compact-citizen-346817.rj.r.appspot.com/api
+  API_URL: 'https://compact-citizen-346817.rj.r.appspot.com/api'
 };
