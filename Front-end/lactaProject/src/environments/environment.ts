20408707ae7128b362118d496aeea8b7a4c35a91
Patricio Labra <patricio.labra827@gmail.com>
2022-09-22 16:03:15 -0300

    se agrega la api

--- Front-end/lactaProject/src/environments/environment.ts
+++ Front-end/lactaProject/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://compact-citizen-346817.rj.r.appspot.com'
+  API_URL: 'https://compact-citizen-346817.rj.r.appspot.com/api'
 };
 
 /*
